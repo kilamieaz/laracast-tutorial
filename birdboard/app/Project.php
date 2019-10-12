e1d7bbedc5be8a2621da94d79ed5517e716fc226
Sultan <im.sultaan05@gmail.com>
2019-10-12 16:03:47 +0700

    test model

--- birdboard/app/Project.php
+++ birdboard/app/Project.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $guarded = []; // yolo
+
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
 }
--- birdboard/resources/views/projects/show.blade.php
+++ birdboard/resources/views/projects/show.blade.php
@@ -1,22 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
 <body>
-    <h1>Birdboard</h1>
-    <ul>
-        @forelse($projects as $item)
-        <li><a href="{{ $item->path() }}">{{$item->title}}</a></li>
-        @empty
-        <li>No Projects yet.</li>
-        @endforelse
-    </ul>
+    <h1>{{ $project->title }}</h1>
+    <div>{{ $project->description }}</div>
 </body>
-
 </html>
