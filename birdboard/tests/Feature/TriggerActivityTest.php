ded33bdc2be8f6b4344c023128af7b067a977adf
Sultan <im.sultaan05@gmail.com>
2019-10-20 12:43:11 +0700

    test activity polymorphic

--- birdboard/tests/Feature/TriggerActivityTest.php
+++ birdboard/tests/Feature/TriggerActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -32,7 +33,10 @@ class TriggerActivityTest extends TestCase
     {
         $project = ProjectFactory::withTasks(1)->create();
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
@@ -44,7 +48,10 @@ class TriggerActivityTest extends TestCase
             'completed' => true
         ]);
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
@@ -64,7 +71,10 @@ class TriggerActivityTest extends TestCase
 
         $project->refresh();
         $this->assertCount(4, $project->activity);
-        $this->assertEquals('incompleted_task', $project->activity->last()->description);
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('incompleted_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
